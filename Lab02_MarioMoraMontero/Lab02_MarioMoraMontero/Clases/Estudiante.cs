2e1d9f995812c76decea36d8d02365e06e0e7df6
MarioMoraMontero-dev <59747282+MarioMoraMontero-dev@users.noreply.github.com>
2020-06-13 11:24:40 -0600

    Clase Estudiante y clase persona

---
+++ Lab02_MarioMoraMontero/Lab02_MarioMoraMontero/Clases/Estudiante.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace Lab02_MarioMoraMontero.Clases
+{
+    
+    public class Estudiante
+    {
+        
+
+        public Estudiante()
+        {
+            Curso cursos = new Curso(null, null);
+
+        }
+
+        public void DefinirCurso(string pNombre, string pNota)
+        {
+            Curso curso = new Curso(pNombre,pNota);
+            
+        }
+
+        public void DefinirPersona(string pNombre, string pPrimerApellido, string pSegundoApellido, int pAnhoNacimiento)
+        {
+
+            Persona pe = new Persona(pNombre, pPrimerApellido, pSegundoApellido, pAnhoNacimiento);
+
+        }
+
+    }
+}
--- Lab02_MarioMoraMontero/Lab02_MarioMoraMontero/Clases/Persona.cs
+++ Lab02_MarioMoraMontero/Lab02_MarioMoraMontero/Clases/Persona.cs
@@ -7,12 +7,23 @@ namespace Lab02_MarioMoraMontero.Clases
 {
     public class Persona
     {
-
+        #region Atributos de la clase
+        /// <summary>
+        /// Atributos de la clase
+        /// </summary>
         public string nombre { get; set; } = "";
         public string primerApellido { get; set; } = "";
         public string segundoApellido { get; set; } = "";
         public int anhoNacimiento { get; set; } = 0;
-
+        #endregion Fin de los atributos de la clase
+        #region Constructor de la clase
+        /// <summary>
+        /// Constructor de la clase
+        /// </summary>
+        /// <param name="pNombre"></param>
+        /// <param name="pPrimerApellido"></param>
+        /// <param name="pSegundoApellido"></param>
+        /// <param name="pAnhoNacimiento"></param>
         public Persona(string pNombre, string pPrimerApellido, string pSegundoApellido, int pAnhoNacimiento)
         {
             this.nombre = pNombre;
@@ -20,14 +31,21 @@ namespace Lab02_MarioMoraMontero.Clases
             this.segundoApellido = pSegundoApellido;
             this.anhoNacimiento = pAnhoNacimiento;
         }
-
-
+        #endregion Fin del constructor
+        #region Método
+        /// <summary>
+        /// Método que devuelve el nombre completo
+        /// </summary>
+        /// <returns></returns>
         public String ObtenerNombreCompleto()
         {
             string NombreCompleto = this.nombre + " " + this.primerApellido + " " + this.segundoApellido;
             return NombreCompleto;
         }
-
+        /// <summary>
+        /// Método que calcula la edad
+        /// </summary>
+        /// <returns></returns>
         public int CalcularEdad()
         {
             int edad;
@@ -42,5 +60,6 @@ namespace Lab02_MarioMoraMontero.Clases
             }
             return edad;
         }
+        #endregion Fin de los método
     }
 }
